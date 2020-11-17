b59c5887e89528f12ba15b73fbe6545b744d9957
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-17 17:39:25 +0200

    Add files via upload

--- oop/lab5/Person.h
+++ oop/lab5/Person.h
@@ -1,8 +1,26 @@
 #pragma once
 #include "Date.h"
+#include <iostream>
+
+using namespace std;
+
 class Person :
     public Date
 {
+private:
+    string name_;
+    string surname_;
 
+public:
+    Person();
+    Person(string, string);
+    Person(string, string, int, int, int);
+
+    string getName();
+    void setName(string);
+
+
+    string getSurname();
+    void setSurname(string);
 };
 
