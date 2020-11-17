ae193f4c42e11523ec7a33d95d5da7b9e7f512c7
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-17 17:39:38 +0200

    Add files via upload

--- oop/lab5/Person.cpp
+++ oop/lab5/Person.cpp
@@ -1,1 +1,36 @@
 #include "Person.h"
+#include <iostream>
+
+using namespace std;
+
+Person::Person() {
+	name_ = "Name";
+	surname_ = "Surname";
+}
+
+Person::Person(string name, string surname) {
+	name_ = name;
+	surname_ = surname;
+}
+
+Person::Person(string name, string surname, int year, int month, int day) : Date(year, month, day) {
+	name_ = name;
+	surname_ = surname;
+}
+
+
+string Person::getName() {
+	return name_;
+}
+
+void Person::setName(string name) {
+	name_ = name;
+}
+
+string Person::getSurname() {
+	return surname_;
+}
+
+void Person::setSurname(string surname) {
+	surname_ = surname;
+}
