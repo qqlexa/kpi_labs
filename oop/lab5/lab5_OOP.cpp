5fae26a3286bd1974d05b90a76d4ac78969b1a84
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-19 19:00:01 +0200

    Add files via upload

---
+++ oop/lab5/lab5_OOP.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Date.h"
+#include "Person.h"
+#include "Author.h"
+
+using namespace std;
+
+
+int main(){
+    Date* today = new Date();
+    today->setDay(2);
+    Date* tomorrow = new Date(today);
+    Date tomorrow2(today);
+
+    std::cout << "Today is " << today->getDay() << "." << today->getMonth() << "." << today->getYear() << endl;
+    std::cout << "Tomorrow is " << tomorrow->getDay() << "." << tomorrow->getMonth() << "." << tomorrow->getYear() << endl;
+    std::cout << "Tomorrow2 is " << tomorrow2.getDay() << "." << tomorrow2.getMonth() << "." << tomorrow2.getYear() << endl;
+
+    Person me("Alex", "Hlav", today);
+    std::cout << "I was born in " << me.getDay() << "." << me.getMonth() << "." << me.getYear() << endl;
+
+    Person he(me);
+    std::cout << "He was born in " << he.getDay() << "." << he.getMonth() << "." << he.getYear() << endl;
+
+    return 0;
+}
