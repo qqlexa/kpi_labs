5231a6140c105c52b1bb4b3144bfe69d29da20e9
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-23 17:52:01 +0200

    Add files via upload

--- oop/lab5/ThesisComposition.cpp
+++ oop/lab5/ThesisComposition.cpp
@@ -1,1 +1,46 @@
 #include "ThesisComposition.h"
+
+ThesisComposition::ThesisComposition() {
+	countLists_ = 0;
+}
+
+ThesisComposition::ThesisComposition(vector<Thesis> composition, Date date, int countLists): Date(date) {
+	setComposition(composition);
+	countLists_ = (countLists > 0) ? countLists : 0;
+}
+
+ThesisComposition::ThesisComposition(ThesisComposition& thesisComposition): Date(thesisComposition.getDate()) {
+
+}
+
+ThesisComposition::ThesisComposition(ThesisComposition* thesisComposition) : Date(thesisComposition->getDate()) {
+
+}
+
+vector<Thesis> ThesisComposition::getComposition() {
+	return composition_;
+}
+
+void ThesisComposition::setComposition(vector<Thesis> composition) {
+	composition_.clear();
+	for (Thesis thesis : composition) {
+		composition_.push_back(thesis);
+	}
+}
+
+void ThesisComposition::appendComposition(Thesis thesis) {
+	composition_.push_back(thesis);
+}
+
+void ThesisComposition::clearComposition() {
+	composition_.clear();
+}
+
+
+int ThesisComposition::getCountLists() {
+	return countLists_;
+}
+
+void ThesisComposition::setCountLists(int countLists){
+	countLists_ = countLists;
+}
--- oop/lab5/ThesisComposition.h
+++ oop/lab5/ThesisComposition.h
@@ -1,8 +1,30 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "Thesis.h"
 #include "Date.h"
+
+using namespace std;
+
 class ThesisComposition :
     public Date
 {
+private:
+    vector<Thesis> composition_;
+    int countLists_;
+public:
+    ThesisComposition();
+    ThesisComposition(vector<Thesis>, Date, int countLists=0);
 
+    ThesisComposition(ThesisComposition&);
+    ThesisComposition(ThesisComposition*);
+
+    vector<Thesis> getComposition();
+    void setComposition(vector<Thesis>);
+    void appendComposition(Thesis);
+    void clearComposition();
+
+    int getCountLists();
+    void setCountLists(int);
 };
 
