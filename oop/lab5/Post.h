6eb66c7d99baccfc39604cd928215d97e28faaae
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-24 11:48:23 +0200

    Add files via upload

---
+++ oop/lab5/Post.h
@@ -0,0 +1,11 @@
+#pragma once
+enum class Post {
+    STUDENT,       // 0
+    SPECIALIST,    // 1
+    MASTER,        // 2
+    POSTGRADUATE,  // 3
+    ASSISTANT,     // 4
+    TEACHER,       // 5
+    DOCENT,        // 6
+    PROFESSOR,     // 7
+};
