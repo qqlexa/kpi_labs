e267b94593bffb8a51421e34dac40a47fd8eaa95
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-24 11:47:00 +0200

    Delete AuthorStudent.cpp

--- oop/lab5/AuthorStudent.cpp
+++
@@ -1,8 +0,0 @@
-#include "AuthorStudent.h"
-
-AuthorStudent::AuthorStudent() {
-}
-AuthorStudent::AuthorStudent(Author& author) : Author(author) {
-}
-AuthorStudent::AuthorStudent(Author* author) : Author(author) {
-}
