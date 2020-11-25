68d9d8bd9f44a077814562343ddb2be248ca6eb1
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-25 09:24:28 +0200

    Add files via upload

---
+++ oop/lab5/#6/Person.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+
+#include "Date.h"
+
+using namespace std;
+
+class Person :
+    virtual public Date
+{
+protected:
+    string firstName_;
+    string secondName_;
+
+public:
+    Person();
+    Person(string, string, int, int, int);
+
+    Person(Person&);
+    Person(Person*);
+
+    string getFirstName();
+    void setFirstName(string);
+
+
+    string getSecondName();
+    void setSecondName(string);
+
+};
+
