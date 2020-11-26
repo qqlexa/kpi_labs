143eac1cc599ecc1c7620002e053b032ef06b771
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-26 21:14:16 +0200

    Add files via upload

---
+++ oop/lab5/#6/lab5_OOP.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+#include "Thesis.h"
+#include "ThesisComposition.h"
+
+using namespace std;
+
+
+int main() {
+
+    Thesis thesis1;
+    thesis1.setDrawName("Cool thesis!");
+    Thesis thesis2(thesis1);
+
+    Thesis thesis3(1, 1, 2000,
+        "Name1", "SurName1", 2, 1, 2000, Post::STUDENT, 
+        "Name2", "SurName2", 3, 1, 2000, Post::MASTER, 
+        "DrawName3", 2, 3, 7.5);
+
+    vector<Thesis> compositions = { thesis1, thesis2, thesis3 };
+
+    ThesisComposition thesisComposition(compositions, 12, 12, 2012);
+    thesisComposition.printCompositionInfo();
+    return 0;
+}
