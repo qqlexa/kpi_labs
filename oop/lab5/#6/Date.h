d5d75e89360abfd90af16176819751d18eb1de26
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-26 21:10:49 +0200

    Add files via upload

---
+++ oop/lab5/#6/Date.h
@@ -0,0 +1,28 @@
+#pragma once
+class Date
+{
+protected:
+	int year_;
+	int month_;
+	int day_;
+public:
+    Date();
+    Date(int, int, int);
+
+    Date(Date&);
+    Date(Date*);
+
+    int getYear();
+    void setYear(int);
+
+    int getMonth();
+    void setMonth(int);
+
+    int getDay();
+    void setDay(int);
+
+    void setDate(int, int, int);
+
+    // virtual void show() = 0;
+};
+
