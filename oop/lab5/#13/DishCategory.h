8239ba0b5d0cfcecfa8751af2c8777aab1588138
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-28 20:16:17 +0200

    Add files via upload

---
+++ oop/lab5/#13/DishCategory.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+enum class DishType {
+	FIRST_COURSE,
+	MAIN_COURSE,
+	DESSERT,
+};
+
+class DishCategory {
+protected:
+	DishType dishType_;
+public:
+	DishCategory();
+	DishCategory(DishType);
+
+	DishType getDishType();
+	void setDishType(DishType);
+};
