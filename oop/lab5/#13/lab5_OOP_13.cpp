6931b3ee2e21f699aa52b74e50411fad4c801f61
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-28 20:16:04 +0200

    Add files via upload

---
+++ oop/lab5/#13/lab5_OOP_13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+#include "Order.h"
+
+using namespace std;
+
+
+int main() {
+    Dish potato;
+    potato.setName("Potato!");
+    potato.setPrice(2);
+
+    Dish potato2(potato);
+    potato2.setPrice(5);
+
+    Dish carrot("Carrot", 10.5, DishType::MAIN_COURSE);
+
+    vector<Dish> dishList = { potato, potato2, carrot };
+    /*
+        Order(vector<Dish>,
+        int, int, int,               // Date
+        std::string, std::string, Sex,
+        WorkCategory);
+    */
+    Order order(dishList, 12, 12, 2012, "Anton", "Kasyanov", Sex::MALE, WorkCategory::HEADWAITER);
+    order.printDishListInfo();
+    return 0;
+}
