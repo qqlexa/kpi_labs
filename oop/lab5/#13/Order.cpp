15744fede36941ea930a90a7dc094c31c5c22095
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-28 20:15:55 +0200

    Add files via upload

---
+++ oop/lab5/#13/Order.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include "Order.h"
+
+using namespace std;
+
+Order::Order() {
+}
+
+Order::Order(vector<Dish> dishList, 
+	int year, int month, int day,  
+	string name, string surname, Sex sex, 
+	WorkCategory workCategory) : Waiter(name, surname, sex, year, month, day, workCategory, year, month, day), 
+								 Date(year, month, day) {
+	setDishList(dishList);
+}
+
+Order::Order(Order& order) {
+	setDishList(order.dishList_);
+}
+
+Order::Order(Order* order) {
+	setDishList(order->dishList_);
+}
+
+vector<Dish> Order::getDishList() {
+	return dishList_;
+}
+
+void Order::setDishList(vector<Dish> composition) {
+	dishList_.clear();
+	for (Dish thesis : composition) {
+		dishList_.push_back(thesis);
+	}
+}
+
+void Order::appendDishList(Dish thesis) {
+	dishList_.push_back(thesis);
+}
+
+void Order::clearDishList() {
+	dishList_.clear();
+}
+
+
+void Order::printDishListInfo() {
+	cout << "Waiter firstname: " << getFirstName() << endl;
+	cout << "Waiter secondname: " << getSecondName() << endl;
+	for (Dish dish : dishList_) {
+		cout << "Dish name: " << dish.getName() << endl;
+		cout << "Price: " << dish.getPrice() << endl;
+		cout << endl;
+	}
+}
