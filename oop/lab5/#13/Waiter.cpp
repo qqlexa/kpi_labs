a316939915b6fe34a26ffff4ec376913cdb45a2f
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-28 20:15:06 +0200

    Add files via upload

---
+++ oop/lab5/#13/Waiter.cpp
@@ -0,0 +1,13 @@
+#include "Waiter.h"
+
+Waiter::Waiter() {
+
+}
+
+Waiter::Waiter(string name, string surname, Sex sex, int year, int month, int day, WorkCategory category, int enrollmentYear, int enrollmentMonth, int enrollmentDay) :
+																										Person(name, surname, sex, year, month, day),
+																										Category(category),
+																										Date(enrollmentYear, enrollmentMonth, enrollmentDay)																									    
+{
+
+}
