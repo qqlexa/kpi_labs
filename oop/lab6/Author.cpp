4be24116d12bdb68ac5db4fe23866ee0a67f5bd2
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-29 22:45:16 +0200

    Add files via upload

---
+++ oop/lab6/Author.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "Author.h"
+
+using namespace std;
+
+Author::Author(){
+	post_ = Post::STUDENT;
+}
+
+Author::Author(string name, string surname, int year, int month, int day, Post post) : Person(name, surname, year, month, day) {
+	post_ = post;
+}
+
+Author::Author(Author& author){
+	post_ = author.post_;
+}
+
+Author::~Author() {
+	show();
+}
+
+Post Author::getPost() {
+	return post_;
+}
+
+void Author::setPost(Post post) {
+	post_ = post;
+}
+
+void Author::show() {
+	cout << "Author.show()" << endl;
+}
