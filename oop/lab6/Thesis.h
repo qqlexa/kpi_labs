f63fa8e164d2aaa43eb534b394eb3e856c5e7634
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-29 22:44:44 +0200

    Add files via upload

---
+++ oop/lab6/Thesis.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+
+#include "Date.h"
+#include "Author.h"
+
+using namespace std;
+
+class Thesis :
+    virtual public Date
+{
+protected:
+    Author authorStudent_;
+    Author authorBoss_;
+    string drawName_;
+    int countDraws_;
+    int countLinks_;
+    float amountLists_;
+public:
+    Thesis();
+    Thesis(int, int, int,                       // Date
+        string, string, int, int, int, Post,    // AuthorStudent
+        string, string, int, int, int, Post,    // AuthorBoss 
+        string, int, int, float);               // name, countDraws, countLinks, amountLists
+
+    Thesis(Thesis&);
+
+    Thesis(const Thesis&);
+    ~Thesis();
+
+    Author& getAuthorStudent();
+    void setAuthorStudent(Author&);
+
+    Author& getAuthorBoss();
+    void setAuthorBoss(Author&);
+
+    string getDrawName();
+    void setDrawName(string);
+
+    int getCountDraws();
+    void setCountDraws(int);
+
+    int getCountLinks();
+    void setCountLinks(int);
+
+    float getAmountLists();
+    void setAmountLists(float);
+
+    virtual void show();
+};
+
